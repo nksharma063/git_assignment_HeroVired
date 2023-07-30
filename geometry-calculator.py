afd4af7c72f8ce54c4d7a31a2b2d7a5909706d40
Neeraj Sharma <nksharma063@gmail.com>
2023-07-31 01:18:45 +0530

    modified the name of the function

--- geometry-calculator.py
+++ geometry-calculator.py
@@ -4,6 +4,6 @@ class GeometryCalculator:
     def __init__(self): 
         self.math=math
         
-    def calculate_cicrcle_area(self, *radius):
+    def calculate_circle_area(self, *radius):
         return math.pi * radius ** 2
     
