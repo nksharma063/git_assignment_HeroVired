5cf301b64574f09b90c1847af7fffda1339df7c6
Neeraj <47555491+nksharma063@users.noreply.github.com>
2023-07-30 16:46:03 +0530

    Update CalculatorPlus.py
    Added main function and print function inside main.

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -1,11 +1,23 @@
-def add(a, b):
+class Calculator(self, a,b):
+  a = self.a
+  b = self.b
+
+def add(self,a, b):
   return a + b
 
-def subtract(a, b):
+def subtract(self, a, b):
   return a - b
 
-def multiply(a, b):
+def multiply(self, a, b):
   return a * b
  
-def divide(a, b):
+def divide(self, a, b):
   return a / b
+
+if  __name__  == "__main__": 
+  calculator = Calculator()
+  num1 = 16
+  num2 = 4
+  print(f"{num1} + {num2} = {calculator.add(num1, num2)}") print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}") print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+ 
