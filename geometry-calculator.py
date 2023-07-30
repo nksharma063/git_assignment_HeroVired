eb449a8947b2286dc62348d66e6121ac69744e9c
Neeraj Sharma <nksharma063@gmail.com>
2023-07-31 00:41:36 +0530

    retrieved stashed file at 0 location and added the code to branch

--- geometry-calculator.py
+++ geometry-calculator.py
@@ -1,4 +1,9 @@
 import math
 
 class GeometryCalculator:
+    def __init__(self): 
+        self.math=math
+        
+    def calculate_cicrcle_area(self, *radius):
+        return math.pi * radius ** 2
     
