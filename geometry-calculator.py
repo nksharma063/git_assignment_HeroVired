eb26fbec7009cbc6c87be414dd418468c01373f4
Neeraj Sharma <nksharma063@gmail.com>
2023-07-31 01:20:21 +0530

    Added the rectangle area fucntion and init fucntion too as itw as empty file

--- geometry-calculator.py
+++ geometry-calculator.py
@@ -1,4 +1,10 @@
 import math
 
 class GeometryCalculator:
+    def __init__(self):
+        self.math = math
+    
+    def calculate_rectangle_area(self, a, b,):
+        return a * b
+        
     
